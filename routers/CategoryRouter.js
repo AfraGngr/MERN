2ec03d5bbbf68b9c370a0772b5f509553b90dc8c
AfraGngr <gungorafra93@gmail.com>
2020-12-16 01:51:56 +0300

    Added middleware and validation

--- routers/CategoryRouter.js
+++ routers/CategoryRouter.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const CategoryController = require('../controllers/CategoryController');
+const validations = require('../middleware/validationMiddleware');
 
 
-router.post('/addCategory', CategoryController.addCategory);
+router.post('/addCategory', validations.categoryValidation, CategoryController.addCategory);
 
 router.get('/getCategory/:id', CategoryController.getCategory);
 
-router.post('/updateCategory', CategoryController.updateCategory);
+router.post('/updateCategory', validations.categoryValidation, CategoryController.updateCategory);
 
 router.get('/deleteCategory/:id', CategoryController.deleteCategory);
 
+router.get('/destroyCategory/:id', CategoryController.destroyCategory);
+
 router.get('/', CategoryController.getCategories);
+
+
+module.exports = router;
